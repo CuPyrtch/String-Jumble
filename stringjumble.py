c92ed6586187e36a0a27caa889404b6e7e6b2b25
CuPyrtch <46973325+CuPyrtch@users.noreply.github.com>
2019-03-30 17:25:10 -0400

    Updated from Brython Server: 3/30/2019 5:25:10 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,9 +1,9 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: CuPyrtch
+Credit: None
 
-Assignment:
+Assignment: String Jumble
 
 The purpose of this challenge is to gain proficiency with 
 manipulating lists.
@@ -25,3 +25,40 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+input1 = input("Please enter a string of text (the bigger the better): ")
+
+length = len(input1)
+output = ""
+for i in range(length-1, -1, -1):
+    output = output + input1[i]
+print(output)
+
+output = ""
+begin = length-1
+end = length
+for i in range(length-1, -1, -1):
+    if input1[i] == " ":
+        begin = i+1
+        output = output + input1[begin:end] + " "
+        end = i
+    elif i == 0:
+        begin = i
+        output = output + input1[begin:end]
+print(output)
+
+output = ""
+begin = 0
+end = 0
+for i in range(0, length):
+    if input1[i] == " ":
+        end = i-1
+        for j in range(end, begin-1, -1):
+            output = output + input1[j]
+        output = output + " "
+        begin = i+1
+    elif i == length-1:
+        end = i
+        for j in range(end, begin-1, -1):
+            output = output + input1[j]
+print(output)
